fba2f785a96f94a2b6cb246ac435abc6864e024c
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-07-28 23:56:53 +0500

    storage fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,10 +123,6 @@ STORAGES = {
     },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-        "OPTIONS": {
-            "timeout": None,
-            "azure_ssl": True,
-        },
     },
 }
 
@@ -228,7 +224,6 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
