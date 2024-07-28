d4c44932abf27767efc4267f36699d2e534bce1d
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-07-28 23:43:29 +0500

    static storage

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -228,6 +228,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
